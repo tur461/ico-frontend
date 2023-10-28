b84c7f78538bcd19cc5bcc3d6216b973933ab7ea
tur461 <tur461.er@gmail.com>
2023-10-29 00:18:29 +0530

    changes - invalid pattern followup corrected in Quotes/index.js

--- src/app/home/Quotes/index.js
+++ src/app/home/Quotes/index.js
@@ -25,32 +25,16 @@ const carouselCards = [
             biggest environmental and societal challenges.
         </span>
 
-        <h5>Tyler Welmans – Blockchain Director, Deloitte Ventures</h5>
-        <span>  Raam Chandrasekharan – WEF Crypto Sustainability Coalition member    </span>
+        <h5>Tyler Welmans </h5>
+        <span> Blockchain Director, Deloitte Ventures </span>
     </div>,
     <div className="quoted" key="3">
         <span>
             Finance is still largely misaligned with climate change goals. The steps forward so far –including those at COP27 – are provisional and do not yet amount to
             an irreversible shift towards a sustainable financial system.
         </span>
-        {/* <span> */}
-        <h5>Nick Robins – Professor in Practice for Sustainable Finance, Grantham Research Institute</h5>
-        {/* </span> */}
-    </div>,
-    <div className="quoted" key="4">
-        <span>
-            The world needs a breakthrough and a new roadmap on climate finance.
-
-        </span>
-        <h5>November 2022 Report of the Independent High-Level Expert Group on Climate Finance</h5>
-    </div>,
-    <div className="quoted" key="5">
-        <span>
-            We believe we are approaching an inflection point, where the promised potential of blockchain will be realised and be measured in billions of users and trillions of dollars in value
-        </span>
-
-        <h5> Money, Tokens and Games – Citigroup</h5>
-
+        <h5>Nick Robins</h5>
+	<span>Professor in practice for sustainable finance, Granthan Research Institute</span>
     </div>,
 ]
 export default function Quotes() {
