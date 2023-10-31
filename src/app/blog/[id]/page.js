1305bf16a1f786a3a93f2d67097c4b3020ec8565
tur461 <tur461.er@gmail.com>
2023-11-01 00:16:04 +0530

    refactor 1

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -37,7 +37,7 @@ const BlogView = () => {
             <p className="p_blog_class_1">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos; standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
-                <p className="p_blog_class_1">Author name here</p>
+                <p className="p_blog_class_1 p-cat_1 author-name">Author name here</p>
                 <p className="p_blog_class_1">28 June 2022</p>
                 </div>
             </div>
